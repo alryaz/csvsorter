b0135abdf3c4831a35c314cefab4693e06b04359
John Sivak <jsivak@morpace.com>
2018-03-04 13:57:55 -0500

    Add support for sort direction and providing key functions for the sort functions to use.

--- csvsorter.py
+++ csvsorter.py
@@ -7,13 +7,14 @@ from multiprocessing import Pool
 class CsvSortError(Exception):
     pass
 
-def csvsort(input_filename, columns, column_types=str, output_filename='', max_size=100, has_header=True, delimiter=',', quoting=csv.QUOTE_MINIMAL, encoding='utf-8', num_parallel=4):
+def csvsort(input_filename, columns, column_types=str, key_funcs=str, output_filename='', max_size=100, has_header=True, delimiter=',', quoting=csv.QUOTE_MINIMAL, encoding='utf-8', num_parallel=4, direction='ascending'):
     """Sort the CSV file on disk rather than in memory
     The merge sort algorithm is used to break the file into smaller sub files and
 
     :param input_filename: the CSV filename to sort
     :param columns: a list of column to sort on (can be 0 based indices or header keys)
     :param column_types: a function or list of functions to cast the sorted columns before sorting. If provided a single function, it is applied to all columns
+    :param key_funcs: a function of lsit of functions to use as the value for **sorting** the columns.
     :param output_filename: optional filename for sorted file. If not given then input file will be overriden.
     :param max_size: the maximum size (in MB) of CSV file to load in memory at once
     :param has_header: whether the CSV contains a header to keep separated from sorting
@@ -29,7 +30,12 @@ def csvsort(input_filename, columns, column_types=str, output_filename='', max_s
     if not isinstance(column_types, list):
       column_types = [column_types] * len(columns)
 
+    # if key_funcs is not a list, makt it one
+    if not isinstance(key_funcs, list):
+        key_funcs = [key_funcs] * len(columns)
+
     assert(len(column_types) == len(columns))
+    assert(len(key_funcs) == len(columns))
 
     # max per parallel sort
     max_size /= num_parallel
@@ -47,11 +53,11 @@ def csvsort(input_filename, columns, column_types=str, output_filename='', max_s
             # sort files in memory concurrently
             filenames = csvsplit(reader, max_size, encoding, tmp_dir)
             with Pool(num_parallel) as pool:
-                tasks = [(memorysort, f, columns, column_types, encoding) for f in filenames]
+                tasks = [(memorysort, f, columns, column_types, key_funcs, encoding, direction) for f in filenames]
                 pool.map(pool_helper, tasks)
 
             # merge sorted files
-            sorted_filename = mergesort(filenames, columns, column_types, tmp_dir=tmp_dir, encoding=encoding, num_parallel=num_parallel)
+            sorted_filename = mergesort(filenames, columns, column_types, key_funcs, tmp_dir=tmp_dir, encoding=encoding, num_parallel=num_parallel, direction=direction)
 
 
         # XXX make more efficient by passing quoting, delimiter, and moving result
@@ -115,14 +121,17 @@ def csvsplit(reader, max_size, encoding, tmp_dir):
     return split_filenames
 
 
-def memorysort(filename, columns, col_types, encoding):
+def memorysort(filename, columns, col_types, key_funcs, encoding, direction):
     """Sort this CSV file in memory on the given columns
     """
     with open(filename, encoding=encoding) as input_fp:
         rows = list(csv.reader(input_fp))
     N = len(columns)
-    func = lambda row : [col_types[n](row[columns[n]]) for n in range(N)]
-    rows.sort(key=func)
+    func = lambda row : [key_funcs[n](row[columns[n]]) for n in range(N)]
+    if direction == 'descending':
+        rows.sort(key=func, reverse=True)
+    elif direction == 'ascending':
+        rows.sort(key=func)
     with open(filename, 'w', newline='\n', encoding=encoding) as output_fp:
         writer = csv.writer(output_fp)
         writer.writerows(rows)
@@ -139,12 +148,12 @@ def yield_csv_rows(filename, columns, col_types, encoding):
     """
     with open(filename, 'r', encoding=encoding) as fp:
         for row in csv.reader(fp):
-            # cast types
+            # cast fields to final desired types
             for c in range(len(columns)):
                 row[columns[c]] = col_types[c](row[columns[c]])
             yield row
 
-def merge(filenames, output, columns, col_types, encoding):
+def merge(filenames, output, columns, col_types, key_funcs, encoding, direction):
     """ Merge 'filenames' into 'output'
     """
     if len(filenames) == 1:
@@ -154,8 +163,12 @@ def merge(filenames, output, columns, col_types, encoding):
     with open(output, 'w', newline='\n', encoding=encoding) as output_fp:
         writer = csv.writer(output_fp)
         rows = (yield_csv_rows(filename, columns, col_types, encoding) for filename in filenames)
-        keyfunc = lambda row: [row[column] for column in columns]
-        writer.writerows(heapq.merge(*rows, key=keyfunc))
+        N = len(columns)
+        keyfunc = lambda row: [key_funcs[n](row[columns[n]]) for n in range(N)]
+        if direction == 'descending':
+            writer.writerows(heapq.merge(*rows, key=keyfunc, reverse=True))
+        elif direction == 'ascending':
+            writer.writerows(heapq.merge(*rows, key=keyfunc))
 
 def pool_helper(args):
     """ A helper function that accepts a tuple whose first element is a function, and the remaining are its positional arguments.
@@ -164,7 +177,7 @@ def pool_helper(args):
     func(*args[1:])
 
 
-def mergesort(sorted_filenames, columns, col_types, nway=2, tmp_dir='', encoding='utf-8', num_parallel=4):
+def mergesort(sorted_filenames, columns, col_types, key_funcs, nway=2, tmp_dir='', encoding='utf-8', num_parallel=4, direction='ascending'):
     """Merge the sorted csv files into a single output file.
     """
     merge_n = 0
@@ -176,7 +189,7 @@ def mergesort(sorted_filenames, columns, col_types, nway=2, tmp_dir='', encoding
         for i in range(0, N, nway):
             files = sorted_filenames[i:min(N,i+nway)]
             outputs.append(os.path.join(tmp_dir, 'merge{}.csv'.format(merge_n)))
-            tasks.append([merge, files, outputs[-1], columns, col_types, encoding])
+            tasks.append([merge, files, outputs[-1], columns, col_types, key_funcs, encoding, direction])
             merge_n += 1
         assert(tasks[-1][1][-1] == sorted_filenames[-1])
 
